9f2368381d61957755a3b075ef04c2d6f8d37f4c
OmidGhavamii <93787288+OmidGhavamii@users.noreply.github.com>
2021-11-08 16:50:06 +0330

    Create Exercise1(Tamrin1).c

---
+++ Exercise1(Tamrin1).c
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <windows.h>
+#include  <stdio.h>
+
+int main() {
+    printf("*****\n");
+    Beep(500,1000);
+    printf("****\n");
+    Beep(500,1000);
+    printf("***\n");
+    Beep(500,1000);
+    printf("**\n");
+    Beep(500,1000);
+    printf("*\n");
+    Beep(500,1000);
+
+
+    return 0;
+}
+//Tamrin Aval
+
