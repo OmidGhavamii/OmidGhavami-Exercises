ccbc3c05a302d7019c74f4ec3b1b1e72d6a7b9a9
Omid Ghavami <93787288+OmidGhavamii@users.noreply.github.com>
2021-11-24 20:02:53 +0330

    Update Exercise5(Tamrin5).c

--- Exercise5(Tamrin5).c
+++ Exercise5(Tamrin5).c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include <stdlib.h>
 int main()
 {
     int a,b;
-    int p1,p2;
+    int *p1,*p2;
     char ch;
-    p1 = &a;
-    p2 = &b;
+    p1=&a;
+    p2=&b;
     printf("Select the Operation\n");
     printf("Type + for Addition\n");
     printf("Type - for Subtraction\n");
@@ -18,21 +17,21 @@ int main()
     switch(ch)
     {
         case '+':
-            printf("%d + %d = %d",a,b,(p1+p2));
+            printf("%d + %d = %d",a,b,(*p1+*p2));
             break;
         case '-':
-            printf("%d - %d = %d",a,b,(p1-p2));
+            printf("%d - %d = %d",a,b,(*p1-*p2));
             break;
         case '*':
-            printf("%d * %d = %d",a,b,(p1*p2));
+            printf("%d * %d = %d",a,b,(*p1**p2));
             break;
         case '/':
-            if(p2==0)
+            if(*p2==0)
             {
                 printf("Sorry, You can not divide a number by 0");
                 return 0;
             }
-            printf("%d / %d = %0.2f",a,b,(p1/(float)p2));
+            printf("%d / %d = %0.2f",a,b,(*p1/(float)*p2));
             break;
         default:
             printf("Sorry, Invalid Choice");
