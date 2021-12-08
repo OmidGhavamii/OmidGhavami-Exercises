182b5e05abeb694fbd66ace57ad465219294db15
Omid Ghavami <93787288+OmidGhavamii@users.noreply.github.com>
2021-12-09 00:18:00 +0330

    Update Exercise8(Tamrin1).c

--- Exercise8/Exercise8(Tamrin1).c
+++ Exercise8/Exercise8(Tamrin1).c
@@ -1,1 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+void rec(char *s);
+void rec2(char *s);
+void rec3(char *s);
+void rec4(char *s);
+#define Max_len 20
+int main() {
+    char str1[Max_len];
+    printf("< Enter the characters >\n");
+    printf("The Max Char input is %d   \n",Max_len);
+    printf("Enter Without space (like 'abc') >>"); gets(str1);
+    printf("\nOne_Element_Subs\n");
+    rec(str1);
+    printf("\nTwo_Element_Subs\n");
+    rec2(str1);
+    printf("\nThree_Element_Subs\n");
+    rec3(str1);
+    printf("\nFour_Element_Subs\n");
+    rec4(str1);
 
+
+    return 0;
+}
+void rec(char *s)
+{
+
+    if (*s == '\0'){
+        return;
+    } else {
+        printf("\n______\n");
+    printf("{'%c'}", *s);
+        printf("\n______\n");
+    rec(s + 1);
+    }
+}
+void rec2(char *s)
+{
+
+    if (*s == '\0'){
+        return ;
+    } else {
+        printf("\n___________________________\n");
+     printf("{'%c'}{'%c'}", *s,*s+1);
+        printf("\n___________________________\n");
+     printf("{'%c'}{'%c'}", *s,*s+2);
+        printf("\n___________________________\n");
+        rec2(s+1);
+       
+    }
+}
+void rec3(char *s)
+{
+
+    if (*s == '\0'){
+        return ;
+    } else {
+        printf("\n________________________________________\n");
+     printf("{'%c'}{'%c'}{'%c'}", *s,*s+1,*s+2);
+        printf("\n________________________________________\n");
+     printf("{'%c'}{'%c'}{'%c'}", *s,*s+2,*s+3);
+        printf("\n________________________________________\n");
+     printf("{'%c'}{'%c'}{'%c'}", *s,*s+3,*s+4);
+        printf("\n________________________________________\n");
+     rec3(s+1);
+
+    }
+}
+void rec4(char *s)
+{
+
+    if (*s == '\0'){
+        return ;
+    } else {
+        printf("\n_____________________________________________________\n");
+     printf("{'%c'}{'%c'}{'%c'}{'%c'}", *s,*s+1,*s+2,*s+3);
+        printf("\n_____________________________________________________\n");
+     printf("{'%c'}{'%c'}{'%c'}{'%c'}", *s,*s+2,*s+3,*s+4);
+        printf("\n_____________________________________________________\n");
+     printf("{'%c'}{'%c'}{'%c'}{'%c'}", *s,*s+3,*s+4,*s+5);
+        printf("\n_____________________________________________________\n");
+     printf("{'%c'}{'%c'}{'%c'}{'%c'}", *s,*s+4,*s+5,*s+6);
+        printf("\n_____________________________________________________\n");
+     rec4(s+1);
+
+    }
+}
